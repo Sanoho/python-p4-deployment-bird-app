012fdcc492e7aba5f00309cbf2b58c1e2634d6a3
Timothy <sanohoang@gmail.com>
2023-04-27 15:37:46 -0500

    commiting all the work before deploying

---
+++ server/migrations/versions/18aa439633a8_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 18aa439633a8
+Revises: c93a14d1aa19
+Create Date: 2023-04-27 15:26:44.960726
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '18aa439633a8'
+down_revision = 'c93a14d1aa19'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
